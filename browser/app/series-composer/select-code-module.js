af16908d0b415b7fce029f1321cdbf26fcb55070
Nicholas Drane <nicholasdrane@gmail.com>
2018-01-26 13:01:52 -0600

    refactor(select codeModule): use PureComponent

--- browser/app/series-composer/select-code-module.js
+++ browser/app/series-composer/select-code-module.js
@@ -1,19 +1,28 @@
 import React from "react";
 import _ from "lodash";
 
-export default function SelectCodeModule({ logData = [], codeModule, handleChange }) {
-  const options = _.uniq(logData.map(line => line.codeModule)).filter(codeModule => codeModule);
+export default class SelectCodeModule extends React.PureComponent {
+  render() {
+    const { codeModule, handleChange } = this.props;
+    const logData = this.props.logData || [];
 
-  return (
-    <select value={codeModule} onChange={e => handleChange(e, "codeModule")}>
-      <option key={`select a codeModule`} value={`select a codeModule`}>{`select a codeModule`}</option>
-      {options.map(option => {
-        return (
-          <option key={option} value={option}>
-            {option}
-          </option>
-        );
-      })}
-    </select>
-  );
+    console.log("rerendering");
+    const options = _.uniq(logData.map(line => line.codeModule)).filter(codeModule => codeModule);
+
+    return (
+      <select value={codeModule} onChange={e => handleChange(e, "codeModule")}>
+        <option
+          key={`select a codeModule`}
+          value={`select a codeModule`}
+        >{`select a codeModule`}</option>
+        {options.map(option => {
+          return (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          );
+        })}
+      </select>
+    );
+  }
 }
